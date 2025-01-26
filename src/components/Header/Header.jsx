1086abb1c98c624c48d61c7880bbf72e41b41944
khaled Abdulhalim <kabdulhalim@reseau.free.fr>
2025-01-26 12:31:46 +0100

    Update navigation styling and active link handling

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,23 @@
-import { Link } from 'react-router-dom';
+import { NavLink, useMatch } from 'react-router-dom';
 import styles from './Header.module.scss';
 
 function Header() {
+  const matchProfile = useMatch('/profile');
+  const matchHomepage = useMatch('/');
+
+  console.log(matchProfile);
+  console.log(matchHomepage);
+
   return (
     <header className={`${styles.header} d-flex flex-row align-items-center`}>
       <div className="flex-fill">
         <strong> React-router </strong>
       </div>
       <ul className={styles.headerList}>
-        <Link to="/">Homepage</Link>
-        <Link to="/profile">Profile</Link>
-        <Link to="/efzef">???</Link>
+        <NavLink end to="/">
+          Homepage
+        </NavLink>
+        <NavLink to="/profile">Profile</NavLink>
       </ul>
     </header>
   );
